953efb85950ddc5dfb7eedbe96632e40d849477c
ErvinChavez <ervinchavez31@gmail.com>
2025-09-16 21:08:33 -0400

    finished the gitConfig side affect exercise

---
+++ ModulesExercise/globalConfig.js
@@ -0,0 +1,3 @@
+//log a message
+//console.log() will automatically return what is in it.
+console.log("Hello everyone in the world!");
---
+++ ModulesExercise/globalConfigApp.js
@@ -0,0 +1,4 @@
+//we only need to import the file name (globalConfig.js) because console.log() automatically returns a value
+import "./globalConfig.js";
+
+console.log("Testing the app..");
