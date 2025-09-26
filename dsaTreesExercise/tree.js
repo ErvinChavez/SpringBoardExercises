511069b86f52c864ffe03fc9b4a33965c894be71
ErvinChavez <ervinchavez31@gmail.com>
2025-09-25 23:17:04 -0400

    finsihed the sumValue

--- dsaTreesExercise/tree.js
+++ dsaTreesExercise/tree.js
@@ -1,22 +1,22 @@
-/** TreeNode: node for a general tree. */
-
-class TreeNode {
-  constructor(val, children = []) {
-    this.val = val;
-    this.children = children;
-  }
-}
-
 class Tree {
   constructor(root = null) {
     this.root = root;
   }
 
   /** sumValues(): add up all of the values in the tree. */
-
   sumValues() {
+    if (this.root === null) return 0;
     
+    function valuesOfNode(node) {
+      let total = node.val;
+
+      for (let child of node.children) {
+        total += valuesOfNode(child);
+      }
+      return valuesOfNode(this.root);
+    }
   }
+
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
--- dsaTreesExercise/tree.test.js
+++ dsaTreesExercise/tree.test.js
@@ -8,6 +8,13 @@ beforeEach(function() {
   emptyTree = new Tree();
 
   // build small tree
+  class TreeNode {
+    constructor(val){
+      this.val = val;
+      this.children = [];
+    }
+  }
+
   let nSmall = new TreeNode(1);
   let nSmall2 = new TreeNode(2);
   nSmall.children.push(nSmall2);
