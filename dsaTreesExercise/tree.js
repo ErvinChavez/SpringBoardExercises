422fec24c1e64980907b9f4ce9c4309b8910cced
ErvinChavez <ervinchavez31@gmail.com>
2025-10-05 10:59:50 -0400

    completed countEven() and updated notes

--- dsaTreesExercise/tree.js
+++ dsaTreesExercise/tree.js
@@ -11,32 +11,47 @@ class Tree {
   constructor(root = null) {
     this.root = root;//the start or root of the tree
   }
+  
+  
+/** sumValues(): add up all of the values in the tree. */
+
     sumValues() {
     let sumOfValues = 0;
-    const values = [this.root];//this should be the the val of the root
+    const values = [this.root];//this are the values of the tree starting at the root
 
-    if (this.root === null) return 0;
+    if (this.root === null) return 0; //if root is null then just return 0
 
-    while (values.length) {
-      const current = values.shift();
-      sumOfValues += current.val;
-      for (let child of current.children) {
-        values.push(child)
+    while (values.length) {//while there is still values remaining in the array
+      const current = values.shift();//looking at the current val
+      sumOfValues += current.val;//adding that val to the sumOfValues
+      for (let child of current.children) {//going down the tree to the children
+        values.push(child)//pushing the next children to the array of values
       }
     } 
-    return sumOfValues;
+    return sumOfValues; //return the sumOfValues (total)
   }
 
-  /** sumValues(): add up all of the values in the tree. */
-  
 
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
-    //now loop through the node
-    //add to counter for every node
-    //return the counter at end now for total
+    let evenVals = []; //array of even values being pushed to
+    const values = [this.root]//Starting at the root of tree
+
+    if (this.root === null) return 0;//return 0 is nothing in root 
+
+    while (values.length) { //the vals in the queue
+      const currentVal = values.shift();//checking the current val
+
+      if (currentVal.val % 2 === 0) { //checking if value is even
+        evenVals.push(currentVal.val);}
+      
+      for (let child of currentVal.children) {//adding the children to the queue to process it next
+        values.push(child);
+      }
+    }
+    return evenVals.length;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
