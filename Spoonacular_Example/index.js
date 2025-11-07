de96425e133730d6f91768e98de7bbd63f665f56
ErvinChavez <ervinchavez31@gmail.com>
2025-11-06 23:08:23 -0500

    API Exercise

--- Spoonacular_Example/index.js
+++ Spoonacular_Example/index.js
@@ -1,1 +1,12 @@
-const spoonacularAPIKEY = "";
+const spoonacularAPIKEY = "cea7ccbc9d564f25906a5058dde3a512";
+
+// axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${spoonacularAPIKEY}`).then( (response) => {
+//     console.log(response.data.recipes[0]);
+// });
+
+
+// complex search //
+
+axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonacularAPIKEY}&cuisine=italian&diet=vegetarian`).then((response) => {
+    console.log(response.data);
+})
