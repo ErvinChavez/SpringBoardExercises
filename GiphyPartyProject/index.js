f96d58fc53b99df26ca59a612a51c82d615bd248
ErvinChavez <ervinchavez31@gmail.com>
2025-11-06 21:11:25 -0500

    tring out API keys

--- GiphyPartyProject/index.js
+++ GiphyPartyProject/index.js
@@ -1,3 +1,16 @@
 // look back at the <readme.md> file for some hints //
 // working API key //
-const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+
+// axios.get("https://dog.ceo/api/breeds/image/random")
+//     .then(function (response){ 
+//         console.log(response.data); 
+//     }) 
+
+// axios.get("https://official-joke-api.appspot.com/random_joke").then((response) => {
+//     console.log(response.data
+//     );
+// })
+
+axios.get("https://api.genderize.io?name=luc").then((response) => {
+    console.log(response.data)
+})
