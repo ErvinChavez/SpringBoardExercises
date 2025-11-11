972eca3f20c14bc5d17a98517b7665b5cc01eb4b
ErvinChavez <ervinchavez31@gmail.com>
2025-11-10 21:53:48 -0500

    spoonacular API Keys Examples

--- Spoonacular_Example/index.js
+++ Spoonacular_Example/index.js
@@ -1,11 +1,12 @@
 const spoonacularAPIKEY = "cea7ccbc9d564f25906a5058dde3a512";
 
+//Using the API Key
 // axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${spoonacularAPIKEY}`).then( (response) => {
 //     console.log(response.data.recipes[0]);
 // });
 
 
-// complex search //
+// complex search (keeping adding to address)//
 
 axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonacularAPIKEY}&cuisine=italian&diet=vegetarian`).then((response) => {
     console.log(response.data);
