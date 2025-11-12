b49d7a150fdd8a766f44ae5d370304cac8f2d9d6
ErvinChavez <ervinchavez31@gmail.com>
2025-11-11 22:48:55 -0500

    added an error return message

--- foodGenerator/index.js
+++ foodGenerator/index.js
@@ -2,7 +2,7 @@
 // click a button ad display that recipe //
 // the browser needs to display it //
 
-const spoonacularAPIKEY = "cea7ccbc9d564f25906a5058dde3a512";
+const spoonacularAPIKEY = "cea7ccbc9d564f25906a5058dde3a512bad"
 
 const button = document.getElementById("generate-button");
 const displayDiv = document.getElementById("display-div");
@@ -13,14 +13,16 @@ button.addEventListener("click", function() {
 });
 
 async function getRandomRecipe() {
+    //adding a new div into the "generated recipe" section of page
+    let newDiv = document.createElement("div");
+    let h3 = document.createElement("h3");
+
+    try {
     const response = await axios.get(`https://api.spoonacular.com/recipes/random?apiKey=${spoonacularAPIKEY}`)
     console.log(response);
     displayDiv.innerHTML = "";
 
-    //adding a new div into the "generated recipe" section of page
     
-    let newDiv = document.createElement("div");
-    let h3 = document.createElement("h3");
 
     //set the recipe title into the header
     h3.innerHTML = response.data.recipes[0].title;
@@ -29,6 +31,21 @@ async function getRandomRecipe() {
     //set the recipe summary into the new div
     newDiv.innerHTML = response.data.recipes[0].summary;
     displayDiv.appendChild(newDiv);
+    } catch (error) {
+        console.log(error);
+        //clear the div
+        displayDiv.innerHTML = "";
+
+        //set the header
+        h3.innerHTML = "Error";
+        h3.classList.add("error-response")
+        displayDiv.appendChild(h3);
+
+        //show the error text from the API response
+        newDiv.innerHTML = error.response.data.message;
+        newDiv.classList.add("error-response");
+        displayDiv.appendChild(newDiv);
+    }
 }
 
 
