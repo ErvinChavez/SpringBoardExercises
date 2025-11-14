a5ced40a56442826574f5a2106729dd674c398a4
ErvinChavez <ervinchavez31@gmail.com>
2025-11-14 06:46:23 -0500

    Given api_key worked

--- GiphyPartyProject/index.js
+++ GiphyPartyProject/index.js
@@ -1,16 +1,54 @@
 // look back at the <readme.md> file for some hints //
 // working API key //
 
-// axios.get("https://dog.ceo/api/breeds/image/random")
-//     .then(function (response){ 
-//         console.log(response.data); 
-//     }) 
+//PRACTICE:
 
 // axios.get("https://official-joke-api.appspot.com/random_joke").then((response) => {
 //     console.log(response.data
 //     );
 // })
 
-axios.get("https://api.genderize.io?name=luc").then((response) => {
-    console.log(response.data)
-})
+//axios.get("https://api.genderize.io?name=ervin").then((response) => {
+//     console.log(response.data)
+// })
+
+// axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${spoonacularAPIKEY}&cuisine=italian&diet=vegetarian`).then((response) => {
+//     console.log(response.data);
+// })
+
+//PROJECT
+
+const spoonacularAPIKEY = "cea7ccbc9d564f25906a5058dde3a512";
+
+//Attemp 1:
+// axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=${spoonacularAPIKEY}`)
+//     .then(function (response){ 
+//         console.log(response); 
+// }) 
+
+//Button Layout
+
+// let button = document.getElementById("submit")
+
+
+// button.addEventListener("click", function() {
+//     console.log("I submited the button")
+// })
+
+//Attempt 2
+
+//Use given code from readme:
+
+//   async function giphyRequest() {
+//     const response = await axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=${spoonacularAPIKEY}`);
+//     console.log(response);
+//   }
+//   giphyRequest();
+//Keep getting a ERR_BAD_REQUEST
+
+//Using given api_key this time:
+  async function giphyRequest() {
+    const response = await axios.get(`http://api.giphy.com/v1/gifs/trending?api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    console.log(response);
+  }
+  giphyRequest();
