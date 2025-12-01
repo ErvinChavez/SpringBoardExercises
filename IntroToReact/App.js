b2c40d48e32377671214620a5ed07ed0bbcbbbe3
ErvinChavez <ervinchavez31@gmail.com>
2025-12-01 13:43:24 -0500

    Conditionals

--- IntroToReact/App.js
+++ IntroToReact/App.js
@@ -1,11 +1,17 @@
 const App = () => {
-    return (
         <div>
+            <Animals 
+                name="Stevie Chicks" 
+                species="Silkie Chicken"
+                isCute= {true}
+                />
             < RandomNum />
             < RandomNum />
             < RandomNum />
+            < Bouncer age={19} />
+            < Bouncer age={11} />
+            < Bouncer age={39} />
         </div>
-    )
 }
 //This is not working: Most likely outdated 
 //ReactDOM.render(<App />, document.getElementById("root"));
