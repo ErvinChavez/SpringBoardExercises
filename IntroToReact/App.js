6b7b24070c01e118b5cc28e8efb908df02fb5dc8
ErvinChavez <ervinchavez31@gmail.com>
2025-12-01 12:24:44 -0500

    random number in react

--- IntroToReact/App.js
+++ IntroToReact/App.js
@@ -1,13 +1,17 @@
 const App = () => {
     return (
         <div>
-            <Shiba />
-            <SharPei />
-            <Shiba />
+            < RandomNum />
+            < RandomNum />
+            < RandomNum />
         </div>
     )
 }
+//This is not working: Most likely outdated 
+//ReactDOM.render(<App />, document.getElementById("root"));
 
+
+//This ensures ReactDOM is loaded before your JSX runs.
 document.addEventListener("DOMContentLoaded", () => {
     ReactDOM.render(<App />, document.getElementById("root"))
 });
