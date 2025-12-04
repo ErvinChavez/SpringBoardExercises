f9bbac482291b32d3aa26b51dd333a5242e12dde
ErvinChavez <ervinchavez31@gmail.com>
2025-12-04 16:02:40 -0500

    start of Pokedex project

---
+++ PokeDex-withReact/App.js
@@ -0,0 +1,7 @@
+/* This should just render a single Pokedex. 
+(It’s common for the top-level app to not have direct logic in it
+, but to render the top application object — 
+this becomes useful when you build sites that compose several
+ different parts together.)*/
+
+ 
