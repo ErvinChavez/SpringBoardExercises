54b6df26d16223d7cc163e7fe54673d1a630d0fd
ErvinChavez <ervinchavez31@gmail.com>
2025-12-05 12:58:05 -0500

    attempt after undestanding defaultProps

--- PokeDex-withReact/Pokedex.js
+++ PokeDex-withReact/Pokedex.js
@@ -2,23 +2,12 @@
 describing different pokemon, and renders a 
 sequence of Pokecard components.*/
 
-function Pokedex() {
-const defaultProps = [
-  {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
-  {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
-  {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
-  {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
-  {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
-  {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
-  {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
-  {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-];
-    
+function Pokedex({pokemon}) {
 return (
   <div>
     <ul>
       { 
-        defaultProps.map((item, index) => (
+        pokemon.map((item, index) => (
           
           <li key={index}>
             <Pokecard 
@@ -34,3 +23,15 @@ return (
   </div>
 );
 }
+Pokedex.defaultProps = {
+    pokemon: [
+  {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
+  {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
+  {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
+  {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
+  {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
+  {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
+  {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
+  {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
+]
+};
