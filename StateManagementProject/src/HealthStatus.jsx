cb300154ebfc1de7e7d6d0515d6c4d4f27d15f6a
ErvinChavez <ervinchavez31@gmail.com>
2025-12-15 00:30:59 -0500

    healthStatus for players component

---
+++ StateManagementProject/src/HealthStatus.jsx
@@ -0,0 +1,14 @@
+//Set Initial Health: Begin your game by setting both the player's 
+// and the enemy's health to 100. 
+// These health values will change based on gameplay actions.
+
+const Health = () => {
+    //Am i using some type of useState here
+    
+
+    return (
+        <div>
+
+        </div>
+    );
+}
