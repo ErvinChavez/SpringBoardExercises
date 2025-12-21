eebf6f743f4572ba80ebc79fd561bb490c9b6eaf
ErvinChavez <ervinchavez31@gmail.com>
2025-12-21 14:56:22 -0500

    Stuck so starting to use Solution as a guide from this point

--- StateManagementProject/src/EndOfGame.jsx
+++ StateManagementProject/src/EndOfGame.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const EndofGame = () => {
     //If game outcomes is a win, show congratulatory message,
-    //A lost message, or 
+    //A lost message, or
     //A draw message
 
     //Hide the "Fire" button and show the "Restart" button
--- StateManagementProject/src/InitialHealth.jsx
+++ StateManagementProject/src/InitialHealth.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-//Set Initial Health: Begin your game by setting both the player's 
-// and the enemy's health to 100. 
-// These health values will change based on gameplay actions.
+//Set Initial Health: Begin your game by setting both the player's
+//and the enemy's health to 100.
+//These health values will change based on gameplay actions.
 
 const InitialHealth = (playersHealth = 100, enemysHealth = 100) => {
     //Am i using some type of useState here
