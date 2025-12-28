45ed9fbb657f59cf6a33d9ee6585a2e6867a5f04
ErvinChavez <ervinchavez31@gmail.com>
2025-12-27 21:27:39 -0500

    notes for project

--- reactdeckeffects/src/App.jsx
+++ reactdeckeffects/src/App.jsx
@@ -16,27 +16,54 @@ import './App.css'
  * The change will be from te button
 */
 
+//function getDeck fetchs the deck, NOT the card, return the deck.id to use same deck
+function getDeck() {
+  fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+      .then((res) => res.json())
+      .then((data) => {
+        deckId.append(data.deck_id);
+      })
+}
+
+//values being used
+const deckId = ""
+const cardCode = ""
+const cardImage = ""
+
+
+//function getCard fetchs the one card, and returns the card image and card code
+function getCard() {
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+  .then((res) => res.json())
+  .then((data) => {
+    cardCode.append(data.cards[0].code) //append the cardCode
+
+  })
+}
+//Get the image, making an image element.
+    const imgElement = document.createElement('img');
+    imgElement.src = data.cards[0].image;
+    imgElement.alt = data.cards[0].code;
+
 
 function App() {
 
-  const [cardDrawn, setDrawCard] = useState();
+//   const [cardDrawn, setDrawCard] = useState();
 
 
  
-    useEffect(() => {
-      fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      })
-    })
-  }
+  //   useEffect(() => {
+  //     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //   })
 
-  return (
-    <>
+  // return (
+  //   <>
       
-    </>
-  )
-
+  //   </>
+  // )
+}
 export default App
-
