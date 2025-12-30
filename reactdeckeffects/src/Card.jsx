06002e7cca16faed9d3ea91ea5e9ecced1d4954d
ErvinChavez <ervinchavez31@gmail.com>
2025-12-30 18:55:56 -0500

    compared to solution to see where i was wrong, which i was definitely

--- reactdeckeffects/src/Card.jsx
+++ reactdeckeffects/src/Card.jsx
@@ -15,21 +15,21 @@
 
 
 import React, { useState } from "react";
-import "./Card.css";
+// import "./Card.css";
 
 /** Single card: just renders the card as received from deck. */
 
-function Card({ name, image }) {
+function Card({ name, image }) { //function that returns the card with it's name and image
   // get these once; it will never be updated for the same card
-  const [{ angle, xPos, yPos }] = useState({
+  const [{ angle, xPos, yPos }] = useState({ //(not really sure how to read this useState) A useState that will create a random angle number for the angle, and a random number for xPos and yPos 
     angle: Math.random() * 90 - 45,
     xPos: Math.random() * 40 - 20,
     yPos: Math.random() * 40 - 20,
   });
 
-  const transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`;
+  const transform = `translate(${xPos}px, ${yPos}px) rotate(${angle}deg)`; //the variable transform will set the xPos, yPos, and the angle for that card
 
-  return <img
+  return <img //returns an image with a className, alt name for image, the src or url of the image, in the style of transform for each of the cards
       className="Card"
       alt={name}
       src={image}
--- reactdeckeffects/src/Deck.jsx
+++ reactdeckeffects/src/Deck.jsx
@@ -16,67 +16,66 @@
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import axios from "axios";
-import "./Deck.css";
+// import "./Deck.css";
 
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck"; //variable for the API url
 /** Deck: uses deck API, allows drawing card at a time. */
 
 function Deck() { //the deck component
-  const [deck, setDeck] = useState(null); //useState for the deck being used currently
-  const [drawn, setDrawn] = useState([]); //useState for the cards that have been drawn
-  const [isShuffling, setIsShuffling] = useState(false); //useState to see if deck is shuffled, start as false
+  const [deck, setDeck] = useState(null); //useState for the deck being used currently, currently null
+  const [drawn, setDrawn] = useState([]); //useState for the cards that have been drawn, currently empty array
+  const [isShuffling, setIsShuffling] = useState(false); //useState to see if deck is shuffled, currently set as false
 
-  useEffect(function loadDeckFromAPI() { //
-    async function fetchData() {
-      const d = await axios.get(`${API_BASE_URL}/new/shuffle/`);
-      setDeck(d.data);
+  useEffect(function loadDeckFromAPI() { //function that is going to use an async function fetchData() (Could I have not just started with an async function??)
+    async function fetchData() { //async function (this waits for the load first I believe so)
+      const d = await axios.get(`${API_BASE_URL}/new/shuffle/`); //variable d is set get the API from axios as a new shuffled deck (Why is axios being used and also why not fetch instead?)
+      setDeck(d.data);//setDeck will get rerendered and make deck equal to the d.data from the API
     }
-    fetchData();
-  }, []);
+    fetchData();//runs fetchData function
+  }, []);//useEffect will only rerender thin once because of the empty [] (I think i just answered my own question, we needed async function inside another to be able to call it after and and be able to use useEffect [] out that funtion to tell it to rerender once)
 
 
 
   /** Draw card: change the state & effect will kick in. */
-  async function draw() {
-    try {
-      const drawRes = await axios.get(`${API_BASE_URL}/${deck.deck_id}/draw/`);
+  async function draw() {//this async function will draw the card
+    try {//this will use the try and catch style to catch any errors that have occur
+      const drawRes = await axios.get(`${API_BASE_URL}/${deck.deck_id}/draw/`);//variable drawRes will get the axios API url for drawing a card, using the same deckID and the draw endpoints
 
-      if (drawRes.data.remaining === 0) throw new Error("Deck empty!");
+      if (drawRes.data.remaining === 0) throw new Error("Deck empty!");//if the remaining for the data is 0, return an error with the Deck Empty!
 
-      const card = drawRes.data.cards[0];
+      const card = drawRes.data.cards[0];//card variable will be the objects inside data.cards
 
-      setDrawn(d => [
-        ...d,
+      setDrawn(d => [ //This one not really sure about, so setDrawn will render and it will run a function named d that returns as a new array of that card drwan with all the values in d (which are objects), but will also return an object with that values for id, name, and image
         {
-          id: card.code,
-          name: card.suit + " " + card.value,
-          image: card.image,
+          id: card.code, //id is set to card.code
+          name: card.suit + " " + card.value,//name is set to card.suit and card.value
+          image: card.image,//image is the image url
         },
       ]);
-    } catch (err) {
-      alert(err);
+    } catch (err) { //if and error is catched
+      alert(err); //return that error as an alert
     }
   }
 
   /** Shuffle: change the state & effect will kick in. */
-  async function startShuffling() {
-    setIsShuffling(true);
-    try {
-      await axios.get(`${API_BASE_URL}/${deck.deck_id}/shuffle/`);
-      setDrawn([]);
-    } catch (err) {
+  async function startShuffling() {//this async function is about the shuffling the same deck 
+    setIsShuffling(true); //setIsShuffling function is set to true
+    try { //run a try, catch, and finally 
+      await axios.get(`${API_BASE_URL}/${deck.deck_id}/shuffle/`); //awaits to get the API using the same deckId to use same deck and adds the endpoint shuffle to shuffle the remaining cards in the deck
+      setDrawn([]);//run the setDrawn function 
+    } catch (err) { //if an error is catched then return that error as an alert in the page
       alert(err);
-    } finally {
+    } finally { //after set the setIsShuffling to false
       setIsShuffling(false);
     }
   }
 
   /** Return draw button (disabled if shuffling) */
-  function renderDrawBtnIfOk() {
-    if (!deck) return null;
+  function renderDrawBtnIfOk() {//this function will render a draw button
+    if (!deck) return null; //if there is no deck return null
 
-    return (
-      <button
+    return ( //return a button
+      <button //button will have a className, onClick with value to run the draw function, disabled set to the isShuffling (not sure what this disable is doing?) disable means “If we’re shuffling, don’t allow any actions”
         className="Deck-gimme"
         onClick={draw}
         disabled={isShuffling}>
@@ -86,10 +85,10 @@ function Deck() { //the deck component
   }
 
   /** Return shuffle button (disabled if already is) */
-  function renderShuffleBtnIfOk() {
-    if (!deck) return null;
-    return (
-      <button
+  function renderShuffleBtnIfOk() { // this function will render a shuffle button
+    if (!deck) return null; //if there is no deck return null
+    return ( //return a button
+      <button  //button will have a className, onClick will run the startShuffling function, disabled set to the isShuffling(not sure what this disable is doing?) disable means “If we’re shuffling, don’t allow any actions”
         className="Deck-gimme"
         onClick={startShuffling}
         disabled={isShuffling}>
@@ -99,14 +98,14 @@ function Deck() { //the deck component
   }
 
   return (
-    <main className="Deck">
+    <main className="Deck" /** return a main with className of Deck*/>
 
-      {renderDrawBtnIfOk()}
-      {renderShuffleBtnIfOk()}
+      {renderDrawBtnIfOk() /** run the renderDrawBtnIfOk to return a draw button*/} 
+      {renderShuffleBtnIfOk()/** run the renderShuffleBtnIfOk to return a shuffle button*/}
 
-      <div className="Deck-cardarea">{
-        drawn.map(c => (
-          <Card key={c.id} name={c.name} image={c.image} />
+      <div className="Deck-cardarea">{  //inside a div with a className of Deck-cardarea
+        drawn.map(c => ( //map through each value in the drawn variable
+          <Card key={c.id} name={c.name} image={c.image} /> //run the Card component with the key, name, and image set to their specfic values
         ))}
       </div>
 
