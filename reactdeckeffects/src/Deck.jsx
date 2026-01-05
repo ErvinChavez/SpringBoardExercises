c580dc74c085c6e18393513a8d0426c28d77e461
ErvinChavez <ervinchavez31@gmail.com>
2026-01-04 21:54:51 -0500

    final with further study

--- reactdeckeffects/src/Deck.jsx
+++ reactdeckeffects/src/Deck.jsx
@@ -119,10 +119,131 @@
 
 //..................................................................................
 
-//FINAL 
+//FINAL (Before Futher Study Part)
 
 
-import React, {useEffect, useState} from "react";
+// import React, {useEffect, useState} from "react";
+// import Card from "./Card";
+// import axios from "axios";
+// import "./Deck.css"
+
+// const API_BaseURL = "https://deckofcardsapi.com/api/deck" //this is the simplest form of the api before andding endpoints regarding what we want to retrieve for the components
+
+
+// export function Deck() { //Ask yourself here, “What data fully describes this screen?”
+//   //These are the 3 UI we want presented
+//   const [deck, setDeck] = useState(null);//Do i have a deck yet? helped by the useEffect to render the deck in
+//   const [drawn, setDrawn] = useState([]);//What cards have been drawn, should have the id, name , and image
+//   const [isShuffling, setIsShuffling] = useState(false); //Am i shuffling//side effect that happen outside normal render
+  
+//   //so now we know to use a useEffect to happen once to load a new deck
+//   //then the new deck loads and rerenders in setDeck so now deck is not an empty array now
+//   useEffect( 
+//     function loadDeckFromAPI() {  
+//       async function fetchData() {
+//         const d = await axios.get(`${API_BaseURL}/new/shuffle/`); //endpoints added to the API link, to retrieve a shuffled deck
+//         setDeck(d.data)//deck is render to equal array of d. data of the shuffled deck
+//       }
+//       fetchData(); //run the function
+//     }, []); //This will render only once
+
+//   //so now that deck is loaded we think what happens when you click the draw button(even though there is no button yet, ask what needs to happen when it is clicked anyways)
+//   async function draw() { //start an async funtion because it would wait for the deck to load 
+//     try {
+//       const drawRes = await axios.get(`${API_BaseURL}/${deck.deck_id}/draw/`); //drawRes is the API url for cards drawn
+
+//       if (drawRes.data.remaining === 0) throw new Error("Deck Empty!"); //If there is no reamining cards, throw an error alert
+
+//       const card = drawRes.data.cards[0]; //card will have all its values for the firs card
+
+//       setDrawn(d => [ // setDrawn is now going to change the drawn array to the values of the card drawn
+//         ...d, {
+//           id: card.code,
+//           name: card.suit + " " + card.value,
+//           image: card.image,
+//         },
+//       ])
+//     } catch (err) { //if an error occurs, return that error as alert
+//       alert(err);
+//     }
+//   }
+
+//   //So now we think what should happen when you click shuffle button
+//   async function startShuffling() { //Essentiality we want to reset the whole card drawing with the same deck but shuffled again
+//     setIsShuffling(true); //first we set isShuffling to true, this disables the draw and shuffle button
+//     try { 
+//       await axios.get(`${API_BaseURL}/${deck.deck_id}/shuffle`); //the API to reshuffle the same deck is called and also resets to 52 now
+//       setDrawn([]); //setDrawn is set back to an empty array to clear the UI of the drawn card previously
+//     } catch(err) { //if any error, return it as an alert
+//       alert(err);
+//     } finally {
+//       setIsShuffling(false); //At the end set isShuffling back to false to show the draw and shuffle button again
+//     }
+//   }
+
+//   //So now we think what should happen when the buttons are visible and clicked
+//   function renderDrawBtnIfOk() {
+//     if (!deck) return null; //if there is no deck return null(no button created)
+
+//     return (
+//       <button
+//         className="Deck-gimme"
+//         onClick={draw} //on click run draw function
+//         disabled = {isShuffling} //disable button if isShuffling is true, so not clickable
+//       >
+//         DRAW
+//       </button>
+//     );
+//   }
+
+//   function renderShuffleBtnIfOk() {
+//     if (!deck) return null; // if there is no deck return null(no button created)
+//     return (
+//       <button
+//         className="Deck-gimme"
+//         onClick={startShuffling} //on click run the startShffling function
+//         disabled={isShuffling} //disable button if isShuffling is true, so not clickable
+//       >
+//         SHUFFLE DECK
+//       </button>
+//     )
+//   }
+
+//   return (
+//     <main className="Deck">
+
+//       {renderDrawBtnIfOk()} 
+//       {renderShuffleBtnIfOk()}
+
+//       <div className="Deck-cardarea"> 
+//         { //So now we ask, what do we want to see on the UI when a card is drawn, 
+//           //So we decided we want to see the name and image, and id
+//           //ID is used to track cards drawn and help React manage the list efficiently
+
+//           drawn.map(c =>(
+//             <Card
+//               key={c.id}
+//               name={c.name}
+//               image={c.image}
+//             />
+//           )
+//           )
+//         }
+//       </div>
+
+//     </main>
+//   );
+// }
+
+// export default Deck;
+
+
+
+//.....................................................................................................
+
+//FINAL (With Futher Study part)
+
+import React, {useEffect, useRef, useState} from "react";
 import Card from "./Card";
 import axios from "axios";
 import "./Deck.css"
@@ -134,8 +255,13 @@ export function Deck() { //Ask yourself here, “What data fully describes this 
   //These are the 3 UI we want presented
   const [deck, setDeck] = useState(null);//Do i have a deck yet? helped by the useEffect to render the deck in
   const [drawn, setDrawn] = useState([]);//What cards have been drawn, should have the id, name , and image
-  const [isShuffling, setIsShuffling] = useState(false); //Am i shuffling//side effect that happen outside normal render
   
+  const [isShuffling, setIsShuffling] = useState(false); //Am i shuffling//side effect that happen outside normal render
+  const [isDrawing, setIsDrawing] = useState(false); //Is it still drawing
+
+  const timeRef = useRef(null); //The useRef that represents the timeInterval
+
+
   //so now we know to use a useEffect to happen once to load a new deck
   //then the new deck loads and rerenders in setDeck so now deck is not an empty array now
   useEffect( 
@@ -145,40 +271,71 @@ export function Deck() { //Ask yourself here, “What data fully describes this 
         setDeck(d.data)//deck is render to equal array of d. data of the shuffled deck
       }
       fetchData(); //run the function
-    }, []); //This will render only once
+    }, [setDeck]); //This will render when there is a change in setDeck 
+
 
   //so now that deck is loaded we think what happens when you click the draw button(even though there is no button yet, ask what needs to happen when it is clicked anyways)
-  async function draw() { //start an async funtion because it would wait for the deck to load 
-    try {
-      const drawRes = await axios.get(`${API_BaseURL}/${deck.deck_id}/draw/`); //drawRes is the API url for cards drawn
+  useEffect(
+    function drawCardAddToDrawn(){
+      async function fetchCard() { //start an async funtion because it would wait for the deck to load 
+        try {
+          const drawRes = await axios.get(`${API_BaseURL}/${deck.deck_id}/draw/`); //drawRes is the API url for cards drawn
 
-      if (drawRes.data.remaining === 0) throw new Error("Deck Empty!"); //If there is no reamining cards, throw an error alert
+        if (drawRes.data.remaining === 0) throw new Error("Deck Empty!"); //If there is no reamining cards, throw an error alert
 
-      const card = drawRes.data.cards[0]; //card will have all its values for the firs card
+          const card = drawRes.data.cards[0]; //card will have all its values for the first card
 
-      setDrawn(d => [ // setDrawn is now going to change the drawn array to the values of the card drawn
-        ...d, {
-          id: card.code,
-          name: card.suit + " " + card.value,
-          image: card.image,
-        },
-      ])
-    } catch (err) { //if an error occurs, return that error as alert
-      alert(err);
-    }
-  }
+          setDrawn(d => [ // setDrawn is now going to change the drawn array to the values of the card drawn
+            ...d, {
+            id: card.code,
+            name: card.suit + " " + card.value,
+            image: card.image,
+            },
+          ]);
+        } catch (err) { //if an error occurs
+            setIsDrawing(false); //isDrawing is set to false
+            alert(err);//return that error as alert
+        }
+      }
+      
+      if (isDrawing && !timeRef.current) {
+        timeRef.current = setInterval(fetchCard, 1000);
+      } else if (!isDrawing && timeRef.current) {
+        stopDrawingCards()
+      }
+
+      function stopDrawingCards() {
+        if (timeRef.current) clearInterval(timeRef.current);
+        timeRef.current = null;
+      }
+
+      return stopDrawingCards
+}, [isDrawing, deck]);
+
 
   //So now we think what should happen when you click shuffle button
-  async function startShuffling() { //Essentiality we want to reset the whole card drawing with the same deck but shuffled again
-    setIsShuffling(true); //first we set isShuffling to true, this disables the draw and shuffle button
-    try { 
-      await axios.get(`${API_BaseURL}/${deck.deck_id}/shuffle`); //the API to reshuffle the same deck is called and also resets to 52 now
-      setDrawn([]); //setDrawn is set back to an empty array to clear the UI of the drawn card previously
-    } catch(err) { //if any error, return it as an alert
-      alert(err);
-    } finally {
-      setIsShuffling(false); //At the end set isShuffling back to false to show the draw and shuffle button again
+  useEffect(function shuffleDeckViaAPI() {
+    async function startShuffling() { //Essentiality we want to reset the whole card drawing with the same deck but shuffled again
+      try { 
+        await axios.get(`${API_BaseURL}/${deck.deck_id}/shuffle/`); //the API to reshuffle the same deck is called and also resets to 52 now
+        setDrawn([]); //setDrawn is set back to an empty array to clear the UI of the drawn card previously
+        setIsDrawing(false);
+        setIsShuffling(false);
+      } catch(err) { //if any error, return it as an alert
+        alert(err);
+      }
     }
+
+    if (isShuffling && deck) startShuffling(deck);
+}, [isShuffling, deck]);
+
+
+  function toggleDraw() {
+    setIsDrawing(auto => !auto);
+  }
+
+  function startShuffling() {
+    return setIsShuffling(true);
   }
 
   //So now we think what should happen when the buttons are visible and clicked
@@ -188,10 +345,10 @@ export function Deck() { //Ask yourself here, “What data fully describes this 
     return (
       <button
         className="Deck-gimme"
-        onClick={draw} //on click run draw function
+        onClick={toggleDraw} //on click run toggleDraw function
         disabled = {isShuffling} //disable button if isShuffling is true, so not clickable
       >
-        DRAW
+        {isDrawing ? "STOP" : "Keep"} DRAWING FOR ME
       </button>
     );
   }
