e148f682f6618d5dd90ca56c928ee521e2196275
ErvinChavez <ervinchavez31@gmail.com>
2026-01-26 13:59:07 -0500

    Homepage content added

---
+++ SpaceTravelProject/src/pages/HomePage.jsx
@@ -0,0 +1,45 @@
+export default function HomePage() {
+    
+    return (
+        <div className="home">
+            <h2>Space Travel: Expanding Horizons Beyond Earth</h2>
+            <div className="journeyToTheFuture">
+                <h3><img src="">🗺</img> Journey into the Future</h3>
+                <div>
+                    <p>In a world where the impossible has become reality, where the stars are no longer out of reach, welcome to the future of humanity's survival and exploration. Withness the evoluton of technology as it transforms barren planetsin thriving havens, all made possible by the wonders of innovation and human detemination.</p>
+                </div>
+            </div>
+            <div className="fromNeglectToInnovation">
+                <h3><img src="">🌍</img> From Neglect to Innovation</h3>
+                <div>
+                    <p>Once the cradle of civilization, Earth now stands as a solemn reminder of the consequences of neglect and environmental decline. But fear not, for the ingenuity of mankind has soared to new heights. With our relentless pursuit of advancement, we have not only healed ours scars but extended our reach across the cosmos.</p>
+                </div>
+            </div>
+            <div className="enterSpaceTravel">
+                <h3><img src="">🚀</img> Enter Space Travel: Where Dreams Take Flight</h3>
+                <div>
+                    <p>Embark on an extraordinary journey with our groundbreaking web application, aptly named "Space Travel." As a commander engineer, the fate of humanity's exodus rests in your capable hands. Prepare to face the ultimate challenge: evacuating humankind from their birthplace and guiding them towards a future among the stars</p>
+                </div>
+            </div>
+            <div className="engineerExplorerLeader">
+                <h3><img src="">🔧</img> Engineer, Explorer, Leader</h3>
+                <div>
+                    <p>Space Travel empowers you to engineer, design, and even dismantle spacecraft. Craft vessels that defy the boundaries of imagination, envisioning a future where life flourishes beyond the stars. But remember, your role extends beyond construction - you are a leader, an explorer, a commander steering humanity's destiny.</p>
+                </div>
+            </div>
+            <div className="universeOfPossibilities">
+                <h3><img src="">🌌</img> A Universe of Possibilities Awaits</h3>
+                <p>Immerse yourself in the trill of exploration as you chart interplanetary courses within our solar system. Seamlessly navigate your fleet of spacecraft, hurtling through the cosmic void from one celestial body to another. The universe becomes your playground, and every planet a potential new home.</p>
+            </div>
+            <footer>
+                <p>The solar system: the new home.</p>
+                <div>
+                    <img>🌍</img>
+                    <img>🚀</img>
+                    <img>🤖</img>
+                    <img>🪐</img>
+                </div>
+            </footer>
+        </div>
+    )
+};
