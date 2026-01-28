2ba74209183dc75c759fd00984ce957c46ae7315
ErvinChavez <ervinchavez31@gmail.com>
2026-01-28 06:46:14 -0500

    used the local API to get the data and also updated the destroy function in order to work with that local API also

--- SpaceTravelProject/src/pages/Spacecrafts.jsx
+++ SpaceTravelProject/src/pages/Spacecrafts.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SpaceCraftCard from "../components/SpacecraftCard";
+import SpaceTravelApi from "../services/SpaceTravelApi";
 
 export default function Spacecrafts() {
 
@@ -13,24 +14,65 @@ export default function Spacecrafts() {
         currentLocation: <int>
     }  So we make a useState that has arrays of objects to get the info we want*/
 
-    const [spaceCrafts, setSpaceCrafts] = useState([
-        {id: 1, name: "Prispax", capacity: 10000},
-        {id: 2, name: "Pelican", capacity: 200000},
-    ])
+    const [spaceCrafts, setSpaceCrafts] = useState([]);
 
     /* After finishing the spaceCraftCard lets make the loading screen, and catch errors*/
     const [isLoading, setIsLoading] = useState(true);//is the page loading right now?
     const [error, setError] = useState(null);//are there any current errors?
 
-
-
-    /* Now we handle the destroy function, it happens here because this is where the useState is at: We basically want to run through the current(I guess previous) array of objects and filter through making a new array but leave out the one with the card that had the id when clicked on */
-    function destroySpacecraft(id) {
-        setSpaceCrafts((prevSpacecrafts) => 
-            prevSpacecrafts.filter((craft) => 
-                craft.id !== id)); 
-    }
     
+    /*So after placing the logic only, of isLoading, error, empty, or with data; 
+    Let's actually fetch the API to replace the mock logic we used just to help setup.
+    We need to use a useEffect because this will run once when the page loads only */
+
+    useEffect(() => {
+        async function loadSpacecrafts() {
+            /*At first set isLoading to true and error to null */
+            setIsLoading(true);
+            setError(null);
+
+            /*Retrieve the Api from the given api file locally.
+            HAVE TO GO BACK AND FIX SOMETHINGS B/C THIS WAS BUILT THINKING WE WOULD FETCH DATA FROM AN API SOURCE NOT LOCALLY*/
+            const response = await SpaceTravelApi.getSpacecrafts();
+
+            /* what happens when you get the data?
+            First check if there is an error.
+            If there isn't then return the data.
+            After set is loading to false*/
+            if (response.isError) {
+                setError("Failed to load spacecrafts")
+            } else {
+                setSpaceCrafts(response.data)
+            } 
+
+            setIsLoading(false);
+        }
+
+        loadSpacecrafts();
+    }, []);
+
+    /* Now we handle the destroy function, it happens here because this is where the useState is at: We basically want to run through the current(I guess previous) array of objects and filter through making a new array but leave out the one with the card that had the id when clicked on.
+    NEED TO FIX THIS B/C API IS FROM LOCAL FILE NOT OUTSIDE SOURCE AND IT ALREADY HAS A DESTROY FUNCTION WE CAN USE */
+    async function destroySpacecraft(id) {
+        setIsLoading(true);
+
+        /*Local API has a destroy function already we can call */
+        const response = await SpaceTravelApi.destroySpacecraftById({id});
+
+        if (response.isError) {
+            setError("Failed to destory spacecraft");
+            setIsLoading(false);
+            return;
+        }
+
+        /*Now we get the new updated list of spaceCrafts after the deletion */
+        const refreshed = await SpaceTravelApi.getSpacecrafts();
+        /*set setSpaceCrafts list to the new list  */
+        setSpaceCrafts(refreshed.data);
+
+        setIsLoading(false);
+    }
+
     return ( //what should the user see on this page (What is needed:)
         <div className="spaceCrafts"> 
             {/* button to the form section to build a new spacecraft */}
