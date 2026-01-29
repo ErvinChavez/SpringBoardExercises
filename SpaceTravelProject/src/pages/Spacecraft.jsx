3a2c9f69e04399a7032141c03d647c1e6dd30454
ErvinChavez <ervinchavez31@gmail.com>
2026-01-28 22:13:10 -0500

    quick update

--- SpaceTravelProject/src/pages/Spacecraft.jsx
+++ SpaceTravelProject/src/pages/Spacecraft.jsx
@@ -1,6 +1,16 @@
+/**Notes: this page handles 1 spacecraft;
+ * reads the id from url
+ * fetch the spacecraft api
+ * handles the same loading and eroor
+ * displays details
+ * allows going back
+ */
+
 export default function Spacecraft() {
     return (
-        <div>
+        <div className="spacecraft">
+            <button onClick={() => navigate(-1)}>Back</button>
+        <h1>Spacecrafts: {id}</h1>
             
         </div>
     )
