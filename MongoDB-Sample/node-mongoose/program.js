2f94d74e618664e096bc9397e57460b1b8d7696f
ErvinChavez <ervinchavez31@gmail.com>
2026-02-14 17:16:06 -0500

    sample1

--- MongoDB-Sample/node-mongoose/program.js
+++ MongoDB-Sample/node-mongoose/program.js
@@ -8,13 +8,21 @@ const runDatabaseQueries = async () => {
 
 
   // Run this query, should get top 5 best rated movies on IMDB
-  const topMovies = await movies.find({ "imdb.rating": { $gt: 8.0 } })
-    .project({ title: 1, year: 1, "imdb.rating": 1 })
-    .sort({ "imdb.rating": -1 })
-    .limit(5)
-    .toArray();
+  // const topMovies = await movies.find({ "imdb.rating": { $gt: 8.0 } })
+  //   .project({ title: 1, year: 1, "imdb.rating": 1 })
+  //   .sort({ "imdb.rating": -1 })
+  //   .limit(5)
+  //   .toArray();
 
-  console.log('Top Rated Movies:', topMovies);
+  // console.log('Top Rated Movies:', topMovies);
+
+  // insert queries here to test them //
+
+  const lucasMovies = await movies.find({  directors: "George Lucas"})
+  .project({title: 1, year: 1, description: 1})
+  .toArray();
+
+  console.log("George Lucus Movies", lucasMovies);
 
   process.exit(0);
 };
