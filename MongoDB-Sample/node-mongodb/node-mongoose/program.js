acbb9e9e1fc48703b156c982ab083bb2a1844e35
ErvinChavez <ervinchavez31@gmail.com>
2026-02-14 22:11:36 -0500

    read query now working

--- MongoDB-Sample/node-mongodb/node-mongoose/program.js
+++ MongoDB-Sample/node-mongodb/node-mongoose/program.js
@@ -1,6 +1,7 @@
 const connect = require("./db");
 
-// //Create
+// //CREATE
+
 // const runCreate = async () => {
 //   const db = await connect();
 //   const users = db.collection("users");
@@ -20,24 +21,46 @@ const connect = require("./db");
 // runCreate();
 
 
-// const runDatabaseQueries = async () => {
+//READ
+
+const runDatabaseQueries = async () => {
   
-//   const db = await connect();
-//   const movies = db.collection('movies');
+  const db = await connect();
+  const movies = db.collection('movies');
 
 
-//   // Run this query, should get top 5 best rated movies on IMDB
-//   const topMovies = await movies.find({ "imdb.rating": { $gt: 8.0 } })
-//     .project({ title: 1, year: 1, "imdb.rating": 1 })
-//     .sort({ "imdb.rating": -1 })
-//     .limit(5)
-//     .toArray();
+  // // Run this query, should get top 5 best rated movies on IMDB
+  // const topMovies = await movies.find({ "imdb.rating": { $gt: 8.0 } })
+  //   .project({ title: 1, year: 1, "imdb.rating": 1 })
+  //   .sort({ "imdb.rating": -1 })
+  //   .limit(5)
+  //   .toArray();
 
-//   console.log('Top Rated Movies:', topMovies);
+  // console.log('Top Rated Movies:', topMovies);
 
-//   process.exit(0);
-// };
+  // //1. Movies by Christopher Nolan
+
+  // const nolanMovies = await movies.find({directors: "Christopher Nolan"})
+  // .project({title: 1, year: 1, genres: 1})
+  // .toArray();
+
+  // console.log("Christopher Nolan Movies", nolanMovies)
+  // process.exit(0);
+
+  // //2.Find movies that include the genre "Action" and sort (descending) them by year.
+
+  // const actionMovies = await movies.find({genres: "Action"})
+  // .sort({year: 1})
+  // .project({title: 1, year: -1})
+  // .limit(10)
+  // .toArray();
+
+  // console.log("Action Movies by Years", actionMovies)
+
+  
+
+};
 
 
-// runDatabaseQueries();
+runDatabaseQueries();
 
