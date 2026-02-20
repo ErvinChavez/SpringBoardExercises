0864d0da99abfb7dd3080e85add605d429d8450f
ErvinChavez <ervinchavez31@gmail.com>
2026-02-19 20:16:56 -0500

    finished the step2.js

---
+++ NodeExercises/NodeExercise1/nodejs_further_process/step2.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const process = require("process");
+const axios = require("axios");
+
+/** read file at path and print it out. */
+
+function cat(path) {
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
+    }
+  });
+}
+
+/** read page at URL and print it out. */
+
+async function webCat(url) {
+  try {
+    let resp = await axios.get(url);
+    console.log(resp.data);
+  } catch (err) {
+    console.error(`Error fetching ${url}: ${err}`);
+    process.exit(1);
+  }
+}
+
+let path = process.argv[2];
+
+if (path.slice(0, 4) === "http") {
+  webCat(path);
+} else {
+  cat(path);
+}
+
+// const fs = require("fs");
+// const process = require("process");
+// const axios = require("axios");
+
+// function cat(path) {
+//   fs.readFile(path, "utf8", function (err, data) {
+//     if (err) {
+//       console.error(`Error reading ${path}: ${err}`);
+//       process.exit(1);
+//     } else {
+//       console.log(data);
+//     }
+//   });
+// }
+
+// async function webCat(url) {
+//   try {
+//     const resp = await axios.get(url);
+//     console.log(resp.data);
+//   } catch (err) {
+//     console.error(`Error fetching ${url}: ${err}`);
+//     process.exit(1);
+//   }
+// }
+
+// const arg = process.argv[2];
+
+// if (!arg) {
+//   console.error("Please provide a file path or URL.");
+//   process.exit(1);
+// }
+
+// if (arg.startsWith("http://") || arg.startsWith("https://")) {
+//   webCat(arg);
+// } else {
+//   cat(arg);
+// }
