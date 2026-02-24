775457b0a63c28dbd55a2b7c839ab143784fe6c8
ErvinChavez <ervinchavez31@gmail.com>
2026-02-24 05:59:49 -0500

    fixing gitHub projects and lessons

--- ModulesExercise/globalConfigApp.js
+++ ModulesExercise/globalConfigApp.js
@@ -1,2 +1,3 @@
-//we only need to import the file name (globalConfig.js) because console.log() automatically returns a value
+//we only need to import the file name (globalConfig.js) 
+// because console.log() automatically returns a value
 import "./globalConfig.js";
