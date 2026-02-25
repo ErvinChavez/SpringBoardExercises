9a5284ab8ff9185703c12ced8aedfb08c8fb080b
ErvinChavez <ervinchavez31@gmail.com>
2026-02-24 23:42:07 -0500

    lessons on routes and requests

---
+++ Node-ExpressExercises/DefiningRoutes-Requests/app.js
@@ -0,0 +1,34 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = process.env.port || 4444;
+const things = require("./routes/things");
+
+app.use(ex[redd])
+app.use("/things", things);
+//use the things.js file to handle
+//endpoints that start with /things
+
+app.get("/", (req, res) => {
+    res.send("hello root")
+});
+
+
+// //you can use a route, if the files are using the same routes
+// app
+//   .route("/things/cars")
+//   .get((req, res) => {})
+//   .post((req, res) => {});
+
+// app
+//   .route("/things/cars/:carid", (req, res) => {})
+//   .get((req, res) => {})
+//   .post((req, res) => {});
+
+app.listen(port, err => {
+    if (err) {
+        return console.log("ERROR",err);
+    }
+    console.log(`Listening on port ${port}`);
+});
+
