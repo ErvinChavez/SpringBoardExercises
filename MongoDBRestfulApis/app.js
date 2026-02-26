b3ef895833e97f63646309ad9785c13d7d6a4a76
ErvinChavez <ervinchavez31@gmail.com>
2026-02-26 18:50:56 -0500

    start MongoDB lesson

---
+++ MongoDBRestfulApis/app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require('mongoose');
+
+//express app
+const app = express();
+
+//connect to mongodb
+const dbURI = 'mongodb+srv://ervinchavez:@test-cluster.itnyavc.mongodb.net/'
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err))
+
+// register view engine
+app.set("view engine", "ejs");
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+//mongoose and mongo sandbox routes
+
+
+//routes
+app.get("/", (req, res) => {
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds starrs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Now to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
+});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
+});
+
+//404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
