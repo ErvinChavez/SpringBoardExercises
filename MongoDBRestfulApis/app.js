ac8414e915b852bb7caf736027ea19ef68bd0713
ErvinChavez <ervinchavez31@gmail.com>
2026-02-26 22:20:02 -0500

    update Mongodb lesson

--- MongoDBRestfulApis/app.js
+++ MongoDBRestfulApis/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require('mongoose');
+const Blog = require('./models/blogs');
 
 //express app
 const app = express();
@@ -19,7 +20,21 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 
 //mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  })
 
+  blog.save()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
 
 //routes
 app.get("/", (req, res) => {
