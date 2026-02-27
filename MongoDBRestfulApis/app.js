97e08c66f46889f2c4d85e8daccb7032aaef24e6
ErvinChavez <ervinchavez31@gmail.com>
2026-02-27 09:48:35 -0500

    added first post request

--- MongoDBRestfulApis/app.js
+++ MongoDBRestfulApis/app.js
@@ -14,10 +14,52 @@ mongoose.connect(dbURI)
 
 // register view engine
 app.set("view engine", "ejs");
-
 //middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
+
+
+//routes
+app.get("/", (req, res) => {
+  res.redirect('blogs');
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
+
+//blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort( { createdAt: -1})
+    .then((result) => {
+      res.render('index', {title: 'All Blogs', blogs: result})
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body)
+
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
+});
+
+//404 page
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
 
 // //mongoose and mongo sandbox routes
 // app.get('/add-blog', (req, res) => {
@@ -53,32 +95,3 @@ app.use(morgan('dev'));
 //     })
 //     })
 // })
-
-//routes
-app.get("/", (req, res) => {
-  res.redirect('blogs');
-});
-
-app.get("/about", (req, res) => {
-  res.render("about", { title: "About" });
-});
-
-//blog routes
-app.get('/blogs', (req, res) => {
-  Blog.find().sort( { createdAt: -1})
-    .then((result) => {
-      res.render('index', {title: 'All Blogs', blogs: result})
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new Blog" });
-});
-
-//404 page
-app.use((req, res) => {
-  res.status(404).render("404", { title: "404" });
-});
