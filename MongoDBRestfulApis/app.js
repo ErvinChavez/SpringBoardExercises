0af4901628922b155d5a70e4eb3b6a85b847defc
ErvinChavez <ervinchavez31@gmail.com>
2026-02-26 22:56:00 -0500

    mongoDb lesson

--- MongoDBRestfulApis/app.js
+++ MongoDBRestfulApis/app.js
@@ -19,61 +19,60 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-//mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-  const blog = new Blog({
-    title: 'new blog2',
-    snippet: 'about my new blog',
-    body: 'more about my new blog'
-  })
+// //mongoose and mongo sandbox routes
+// app.get('/add-blog', (req, res) => {
+//   const blog = new Blog({
+//     title: 'new blog2',
+//     snippet: 'about my new blog',
+//     body: 'more about my new blog'
+//   })
 
-  blog.save()
-    .then((result) => {
-      res.send(result)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
+//   blog.save()
+//     .then((result) => {
+//       res.send(result)
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+// })
 
-app.get('/all-blogs', (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => console.log(err))
-})
+// app.get('/all-blogs', (req, res) => {
+//   Blog.find()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => console.log(err))
+// })
 
-app.get('/single-blog', (req, res) => {
-  Blog.findById('69a10f2986647616132482bc')
-    .then((result) => {
-      res.send(result)
-    .catch((err) => {
-      console.log(err);
-    })
-    })
-})
+// app.get('/single-blog', (req, res) => {
+//   Blog.findById('69a10f2986647616132482bc')
+//     .then((result) => {
+//       res.send(result)
+//     .catch((err) => {
+//       console.log(err);
+//     })
+//     })
+// })
+
 //routes
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds starrs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Now to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("index", { title: "Home", blogs });
+  res.redirect('blogs');
 });
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
+
+//blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort( { createdAt: -1})
+    .then((result) => {
+      res.render('index', {title: 'All Blogs', blogs: result})
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
 
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new Blog" });
