3eb777c1fba9cc61b3cb059feaafbf83310e5671
ErvinChavez <ervinchavez31@gmail.com>
2026-03-01 18:03:14 -0500

    password issues being fixed

--- ExpressBookstoreProject/config.js
+++ ExpressBookstoreProject/config.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 let DB_URI;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql://postgres:YOUR_PASSWORD@localhost:5432/books_test";
+  DB_URI = `postgresql://postgres:${encodeURIComponent(process.env.DB_PASSWORD)}@localhost:5432/books_test`;
 } else {
-  DB_URI = "postgresql://postgres:YOUR_PASSWORD@localhost:5432/books";
+  DB_URI = `postgresql://postgres:${encodeURIComponent(process.env.DB_PASSWORD)}@localhost:5432/books`;
 }
 
 module.exports = { DB_URI };
