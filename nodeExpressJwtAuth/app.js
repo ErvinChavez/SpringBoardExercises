7e8d8c30aca9c5e6768506d91a2708c8d0bb6618
ErvinChavez <ervinchavez31@gmail.com>
2026-03-02 19:06:34 -0500

    Node Express JWT Auth lesson

---
+++ nodeExpressJwtAuth/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(3000, () => console.log("Server running"));
+  })
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
